fcac68eca1632e22c398b6b3dbb23798185a7e68
kyriten <okysaputraa10@gmail.com>
2023-09-11 00:23:07 +0700

    update parts: change bg color to #991b1b

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => ' lg:inline-block py-2 px-6 bg-red-500 hover:bg-red-600 text-sm text-white tracking-wide font-semibold rounded-xl transition duration-200 uppercase']) }}>
     {{ $slot }}
 </button>
