94e7e42838eb13414eadbf73b73f889161d244fa
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-20 01:42:37 +0700

    new components: add app logo tagline base & small

---
+++ resources/views/components/application-logo-tagline-base.blade.php
@@ -0,0 +1,9 @@
+<div class="grid justify-items-center py-14">
+    <h1 class="text-center font-bold lg:text-9xl text-6xl" data-text="i">tellm</h1>
+    <blockquote class="lg:text-[26px]/[24px] text-[12px]/[10px] italic tracking-wider justify-self-center">
+        keep an
+        eye on
+        your
+        asset
+    </blockquote>
+</div>
---
+++ resources/views/components/application-logo-tagline-small.blade.php
@@ -0,0 +1,7 @@
+<div class="grid mb-2">
+    <h1 class="text-start font-bold text-5xl" data-text="i">tellm</h1>
+    <blockquote class="text-[10px] italic tracking-wider justify-self-start">keep an eye on
+        your
+        asset
+    </blockquote>
+</div>
