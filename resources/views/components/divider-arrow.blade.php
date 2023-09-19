3d8701dbdb4221d59d22e135627bf4d31a67fc6d
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-19 22:38:08 +0700

    update: adding new components (divider)

---
+++ resources/views/components/divider-arrow.blade.php
@@ -0,0 +1,5 @@
+<div class="arrow-divider">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path class="shape-fill" d="M649.97 0L550.03 0 599.91 54.12 649.97 0z"></path>
+    </svg>
+</div>
---
+++ resources/views/components/divider-curve-asymmetrical.blade.php
@@ -0,0 +1,6 @@
+<div class="curve-divider-asymmetrical">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path class="shape-fill" d="M0,0V6c0,21.6,291,111.46,741,110.26,445.39,3.6,459-88.3,459-110.26V0Z">
+        </path>
+    </svg>
+</div>
