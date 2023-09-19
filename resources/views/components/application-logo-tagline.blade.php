b7ca804a6ad363ec9f86ae0e054ca3cda2922912
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-19 22:23:03 +0700

    add: new components

---
+++ resources/views/components/application-logo-tagline.blade.php
@@ -0,0 +1,5 @@
+<div class="block text-center">
+    <h1 class="font-bold text-8xl md:text-9xl" data-text="i">tellm</h1>
+    <blockquote class="text-lg md:text-2xl italic tracking-wider">keep an eye on your asset
+    </blockquote>
+</div>
---
+++ resources/views/components/welcome-animation.blade.php
@@ -0,0 +1,5 @@
+<div class="txt-heading absolute block text-center">
+    <h1 class="font-bold text-7xl" id="txt-welcome" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">WELCOME
+    </h1>
+    <h1 class="font-bold text-7xl" id="txt-to" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">CLIENT</h1>
+</div>
