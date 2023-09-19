cc9e7879a6bf003850fa6c2a423c3af9431058e6
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-20 01:43:13 +0700

    new component: welcome animation

--- resources/views/components/welcome-animation.blade.php
+++ resources/views/components/welcome-animation.blade.php
@@ -1,5 +1,7 @@
 <div class="txt-heading absolute block text-center">
-    <h1 class="font-bold text-7xl" id="txt-welcome" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">WELCOME
+    <h1 class="font-bold lg:text-7xl text-4xl" id="txt-welcome"
+        style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">WELCOME
     </h1>
-    <h1 class="font-bold text-7xl" id="txt-to" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">CLIENT</h1>
+    <h1 class="font-bold lg:text-7xl text-4xl" id="txt-to" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">
+        CLIENT</h1>
 </div>
