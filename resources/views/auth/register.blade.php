380b8b71d81fd6ee02ccbd101c0d7988ec736cfe
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-21 11:23:11 +0700

    update: add new field (position, branch)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,40 +2,72 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
-                autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        <div class="flex flex-wrap">
+            <!-- Name -->
+            <div class="md:w-1/2 ">
+                <x-input-label for="name" :value="__('Name')" />
+                <x-text-input class="block mt-1 w-full" id="name" name="name" type="text" :value="old('name')"
+                    required autofocus autocomplete="name" placeholder="Abimanyu" />
+                <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <!-- Email Address -->
+            <div class="md:w-1/2 md:pl-3">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input class="block mt-1 w-full" id="email" name="email" type="email" :value="old('email')"
+                    required autocomplete="username" placeholder="example@mail.com" />
+                <x-input-error class="mt-2" :messages="$errors->get('email')" />
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <!-- Password -->
+            <div class="mt-4 md:w-1/2">
+                <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                autocomplete="new-password" />
+                <x-text-input class="block mt-1 w-full" id="password" name="password" type="password" required
+                    autocomplete="new-password"
+                    placeholder="&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <x-input-error class="mt-2" :messages="$errors->get('password')" />
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Confirm Password -->
+            <div class="mt-4 md:w-1/2 md:pl-3">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
-                name="password_confirmation" required autocomplete="new-password" />
+                <x-text-input class="block mt-1 w-full" id="password_confirmation" name="password_confirmation"
+                    type="password" required autocomplete="new-password"
+                    placeholder="&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;&#x2022;" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                <x-input-error class="mt-2" :messages="$errors->get('password_confirmation')" />
+            </div>
+
+            <!-- Job Title -->
+            <div class="mt-4 md:w-1/2">
+                <x-input-label for="position" :value="__('Position')" />
+                <x-text-input class="block mt-1 w-full" id="position" name="position" type="text" :value="old('position')"
+                    required autocomplete="position" placeholder="Programmer" />
+                <x-input-error class="mt-2" :messages="$errors->get('Position')" />
+            </div>
+
+            <!-- Branch -->
+            <div class="mt-4 md:w-1/2 md:pl-3">
+                <x-input-label for="branch" :value="__('Branch')" />
+
+                <div class="relative">
+                    <x-select class="appearence-none block mt-1 w-full" id="branch" name="branch">
+                        <option value="" selected disabled hidden>
+                            {{ __('Select Branch') }}</option>
+
+                        <option value="Bogor">Bogor</option>
+                        <option value="Depok">Depok</option>
+                        <option value="Cibubur">Cibubur</option>
+                        <option value="Cimanggis">Cimanggis</option>
+                        <option value="Parung">Parung</option>
+                        <option value="Pamulang">Pamulang</option>
+                    </x-select>
+                </div>
+                <x-input-error class="mt-2" :messages="$errors->get('branch')" />
+            </div>
         </div>
 
         <div class="flex items-center justify-end mt-4">
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,27 +13,33 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form class="mt-6 space-y-6" method="post" action="{{ route('profile.update') }}">
         @csrf
         @method('patch')
 
+        <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
+            <x-text-input class="mt-1 block w-full" id="name" name="name" type="text" :value="old('name', $user->name)"
+                required autofocus autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
+        <!-- Email Address-->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
+            <x-text-input class="mt-1 block w-full" id="email" name="email" type="email" :value="old('email', $user->email)"
+                required autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
                     <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
                         {{ __('Your email address is unverified.') }}
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                        <button
+                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            form="send-verification">
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
@@ -47,18 +53,38 @@
             @endif
         </div>
 
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
-            @endif
+        <!-- Job Title -->
+        <div>
+            <x-input-label for="position" :value="__('position')" />
+            <x-text-input class="mt-1 block w-full" id="position" name="position" type="text" :value="old('position', $user->position)"
+                required autofocus autocomplete="position" />
+            <x-input-error class="mt-2" :messages="$errors->get('position')" />
         </div>
+
+        <!-- Branch -->
+        <div>
+            <x-input-label for="branch" :value="__('branch')" />
+            <div class="relative">
+                <x-select class="block mt-1 w-full" id="branch" name="branch">
+                    <option value="old('branch', $user - > branch)" selected disabled hidden></option>
+
+                    <option value="Bogor">Bogor</option>
+                    <option value="Depok">Depok</option>
+                    <option value="Cibubur">Cibubur</option>
+                    <option value="Cimanggis">Cimanggis</option>
+                    <option value="Parung">Parung</option>
+                    <option value="Pamulang">Pamulang</option>
+                </x-select>
+                <x-input-error class="mt-2" :messages="$errors->get('branch')" />
+            </div>
+
+            <div class="flex items-center gap-4">
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+                @if (session('status') === 'profile-updated')
+                    <p class="text-sm text-gray-600 dark:text-gray-400" x-data="{ show: true }" x-show="show"
+                        x-transition x-init="setTimeout(() => show = false, 2000)">{{ __('Saved.') }}</p>
+                @endif
+            </div>
     </form>
 </section>
