2f934dfded625fbfad241a8f7dd5caf641d28f02
asnyuuki <78668567+ossascyber@users.noreply.github.com>
2023-09-21 11:21:44 +0700

    update: add route name for landing page

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landingpage');
-});
+})->name('landing-page');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
