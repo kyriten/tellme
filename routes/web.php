614f45173279a944d5d417941ad6cb22e9b67edf
Abimanyu Okysaputra Rachman <78668567+kyriten@users.noreply.github.com>
2023-09-25 18:56:40 +0700

    Role permission (#3)
    * spatie: setup role permission
    
    * spatie: add admin role
    
    * spatie: create admin layout
    
    * spatie: change UserSeeder to EmployeeSeeder
    
    * spatie: new role 'user'
    
    * just update comment for spatie & breeze default
    
    ---------
    
    Co-authored-by: kyriten <78668567+kyriten@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -18,22 +18,24 @@ Route::get('/', function () {
     return view('landingpage');
 })->name('landing-page');
 
+// Spatie: Role and Permission
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin', function () {
+    return view('admin.index');
+})->middleware(['auth', 'role:admin'])->name('admin.index');
+
+// Breeze Default
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'role:admin'])->name('admin.index');
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['role:user', 'auth', 'verified'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
