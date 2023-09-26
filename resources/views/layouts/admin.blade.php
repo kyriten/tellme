ee14f28a32b0593568a381cfff9bcfb6df187f9d
Abimanyu Okysaputra Rachman <78668567+kyriten@users.noreply.github.com>
2023-09-26 11:20:35 +0700

    admin: layouting top and side nav (#5)
    Co-authored-by: kyriten <78668567+kyriten@users.noreply.github.com>

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,11 +9,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-<<<<<<< HEAD
+
     <link rel="preconnect" href="https://fonts.bunny.net">
-=======
     <link href="https://fonts.bunny.net" rel="preconnect">
->>>>>>> 568076c9b5594c148d8a7964998eb9b00810daa0
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Favicon -->
@@ -21,30 +19,21 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-<<<<<<< HEAD
-=======
 
     <!-- Another CSS -->
     <link href="/dist/tailwind.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" rel="stylesheet" />
->>>>>>> 568076c9b5594c148d8a7964998eb9b00810daa0
 </head>
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-<<<<<<< HEAD
-        @include('layouts.navigation')
-=======
         @include('layouts.admin-topnav')
         @include('layouts.admin-sidenav')
->>>>>>> 568076c9b5594c148d8a7964998eb9b00810daa0
 
         <!-- Page Content -->
         <main>
             {{ $slot }}
         </main>
-<<<<<<< HEAD
-=======
 
         <script type="text/javascript">
             function dropdown() {
@@ -57,7 +46,6 @@
                 document.querySelector(".sidebar").classList.toggle("hidden");
             }
         </script>
->>>>>>> 568076c9b5594c148d8a7964998eb9b00810daa0
     </div>
 </body>
 
