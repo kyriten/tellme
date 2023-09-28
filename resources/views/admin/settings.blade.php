47f858f1a422d89837109e8bb9b30350428273cd
Abimanyu Okysaputra Rachman <78668567+kyriten@users.noreply.github.com>
2023-09-28 23:04:19 +0700

    admin: layouting features (#8)
    * update: add new feature
    
    * admin: layouting features
    
    * admin: adjust mb into 1 rem between menu group
    
    ---------
    
    Co-authored-by: kyriten <78668567+kyriten@users.noreply.github.com>

--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -2,8 +2,8 @@
     <div class="flex-1 px-4 py-2">
 
         <!-- Header -->
-        <header class="bg-white dark:bg-gray-800 shadow mb-4 ">
-            <div class="max-w-7xl py-4 px-4 sm:px-6 lg:px-8 block md:flex md:justify-around items-center">
+        <header>
+            <div class="py-4 px-4 sm:px-6 lg:px-8 block md:flex md:justify-around items-center">
 
                 <!-- Header Title -->
                 <h2 class="font-semibold text-xl flex-1 text-gray-800 dark:text-gray-200 leading-tight">
@@ -11,5 +11,14 @@
                 </h2>
             </div>
         </header>
+
+        <!-- Content -->
+        <main class="bg-white dark:bg-gray-800 shadow mr-4 mb-4 px-4 py-4 min-h-screen items-center block rounded-md">
+
+            <!-- Records -->
+            <div class="bg-white min-h-screen rounded-lg flex flex-col justify-around items-center mt-6">
+                <h2>Settings is Here</h2>
+            </div>
+        </main>
     </div>
 </x-admin-layout>
