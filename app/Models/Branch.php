99fd62a4827e16723962890f99d221a67e23ba36
kyriten <okysaputraa10@gmail.com>
2023-10-18 11:21:21 +0700

    update relationship branch into user

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -14,8 +15,8 @@ class Branch extends Model
         'name'
     ];
 
-    public function user(): HasMany
+    public function users() : HasMany
     {
-        return $this->hasMany((Employee::class));
+        return $this->hasMany(User::class);
     }
 }
