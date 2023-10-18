f78f30b2754b617c2e9fb1eba26f9c1cadb71fbb
kyriten <okysaputraa10@gmail.com>
2023-10-18 11:22:07 +0700

    add faker records

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Branch;
 use Illuminate\Support\Str;
 
 /**
@@ -17,11 +19,22 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $branchId = Branch::inRandomOrder()->first()->id;
+        $gender = fake()->randomElement(['male', 'female']);
+        $position = fake()->randomElement(['head office', 'trainer', 'staff hrd', 'staff multimedia', 'instructor', 'service advisor', 'supervisor', 'front counter/front desk', 'branch head', 'administration', 'staff accounting', 'foreman']);
+        $department = fake()->randomElement(['Training Center', 'IT', 'Accounting', 'Administration', 'SA Office', 'Manager Office', 'Branch Office']);
+
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
+            'fullname' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('12345678'), // password
+            'phone' => fake()->phoneNumber,
+            'gender' => $gender,
+            'branch_id' => $branchId,
+            'department' => $department,
+            'position' => $position,
             'remember_token' => Str::random(10),
         ];
     }
